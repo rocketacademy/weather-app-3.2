50224290c327a74d6f99c78eef7ad5f4dd1e8a20
Chxshire22 <akuindra22@gmail.com>
2023-12-07 02:46:30 +0800

    done base

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,50 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 function App() {
-  const key = '60f5db867acf01e9b394eb0774a23465'
+  const [city, setCity] = useState("");
+  const [displayCity, setDisplayCity] = useState("");
+  const [displayWeather, setDisplayWeather] = useState("");
+  const [displayTemp, setDisplayTemp] = useState("");
+  // const [displayTime, setDisplayTime] = useState("");
 
-  // const API = https://api.openweathermap.org/geo/1.0/direct?q=singapore&limit=1&appid=60f5db867acf01e9b394eb0774a23465
+  const geoApi = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=60f5db867acf01e9b394eb0774a23465`;
+  // const weatherApi = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=60f5db867acf01e9b394eb0774a23465`;
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .get(geoApi)
+      .then((response) => response.data[0])
+      .then((response) => {
+        return axios.get(
+          `https://api.openweathermap.org/data/2.5/weather?lat=${response.lat}&lon=${response.lon}&appid=60f5db867acf01e9b394eb0774a23465`
+        )
+      })
+      .then(response => {
+        console.log(response.data);
+        setDisplayCity(response.data.name)
+        setDisplayWeather(
+          `${response.data.weather[0].main}, ${response.data.weather[0].description}`
+        )
+        setDisplayTemp(
+          `${(Number(response.data.main.temp) - 273.15).toFixed(2)} \u00B0 C`
+        );
+      })
+    setCity("")
+  };
 
-  // const [weather, setWeather] = useState("")
-  const handleSubmit=()=>{
-
-  }
-
-  let Result = () =>{
+  let Result = () => {
     return (
       <div className="result">
-        <h2 className="cityName result-header">Singapore</h2>
+        <h2 className="cityName result-header">{displayCity}</h2>
         <div className="result-content">
-          <p>Weather: HOT AS BALLS </p>
-          <p>Temperature: 1000C</p>
+          <p>Current weather: {displayWeather}</p>
+          <p>Current temperature: {displayTemp}</p>
         </div>
       </div>
-    )
-  }
+    );
+  };
+
   return (
     <>
       <div>
@@ -35,12 +58,19 @@ function App() {
         {/* Follow the weather app instructions on the gitbook to implement this exercise */}
         <form onSubmit={handleSubmit} action="" method="get">
           <label htmlFor="">City: </label>
-          <input type="text" name="" id="" />
+          <input
+            onChange={(e) => setCity(e.target.value.toLowerCase().trim())}
+            type="text"
+            name=""
+            id=""
+            autoFocus
+            value={city}
+            placeholder="Enter city"
+          />
           <button type="submit">Search</button>
         </form>
 
-        <Result/>
-
+        {displayCity? <Result />:null}
       </div>
     </>
   );
