9c63e7980690a9ff47573979b2e856c314b455cb
jasperteo <jaspertzj@outlook.sg>
2023-12-06 16:06:48 +0800

    completed comfortable

--- src/weather.jsx
+++ src/weather.jsx
@@ -1,39 +1,54 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 // import useSWR from "swr";
 
 const API_KEY = import.meta.env.VITE_API_KEY;
 const units = "metric";
 
 export default function Weather({ city }) {
-  const [data, setData] = useState(null);
+  const [weatherData, setWeatherData] = useState(null);
+  const [forecastData, setForecastData] = useState(null);
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    const getData = () =>
-      axios
+    const getData = () => {
+      let lat = "";
+      let lon = "";
+      return axios
         .get(
           `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`
         )
         .then((locationResponse) => {
-          // const lat = locationResponse.data[0].lat;
-          // const lon = locationResponse.data[0].lon;
-          const { lat, lon } = locationResponse.data[0];
+          lat = locationResponse.data[0].lat;
+          lon = locationResponse.data[0].lon;
           return axios.get(
             `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=${units}&appid=${API_KEY}`
           );
         })
-        .then((weatherResponse) => setData(weatherResponse))
+        .then((weatherResponse) => {
+          setWeatherData(weatherResponse);
+          return axios.get(
+            `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=${units}&appid=${API_KEY}`
+          );
+        })
+        .then((forecastResponse) => setForecastData(forecastResponse))
         .catch((error) => setError(error))
         .finally(() => setIsLoading(false));
+    };
     getData();
   }, [city]);
 
   useEffect(() => {
-    console.log(data);
-    return () => console.clear();
-  }, [data]);
+    console.log(weatherData);
+  }, [weatherData]);
 
   // const { data, error, isLoading} = useSWR(city, async () => {
   //   const locationResponse = await axios.get(
@@ -54,26 +69,70 @@ export default function Weather({ city }) {
       <div>DATA HERE!</div>
       {isLoading && <LoadingMessage />}
       {error && <ErrorMessage error={error} />}
-      {data && <DataMessage data={data} city={city} />}
+      {weatherData && forecastData && (
+        <DataMessage
+          weatherData={weatherData}
+          forecastData={forecastData}
+          city={city}
+        />
+      )}
     </div>
   );
 }
 
 const LoadingMessage = () => <code>Loading...</code>;
 const ErrorMessage = ({ error }) => <code>{error}</code>;
-const DataMessage = ({ data, city }) => (
-  <>
-    {/* <code>{JSON.stringify(data, null, 2)}</code> */}
-    <div className="align-left">
-      <p>City: {city}</p>
-      <p>
-        Condtion: {data.data.weather[0].description}
-        <img
-          src={`https://openweathermap.org/img/wn/${data.data.weather[0].icon}.png`}
-          alt="icon"
-        />
-      </p>
-      <p>Temperature: {data.data.main.temp} °C</p>
-    </div>
-  </>
-);
+const DataMessage = ({ city, weatherData, forecastData }) => {
+  return (
+    <>
+      {/* <code>{JSON.stringify(forecastData, null, 2)}</code> */}
+      <div className="align-left">
+        <p>Now</p>
+        <p>City: {city}</p>
+        <p>
+          Condtion: {weatherData.data.weather[0].description}
+          <img
+            src={`https://openweathermap.org/img/wn/${weatherData.data.weather[0].icon}.png`}
+            alt="icon"
+          />
+        </p>
+        <p>Temperature: {weatherData.data.main.temp} °C</p>
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Date</TableCell>
+                <TableCell align="right">City</TableCell>
+                <TableCell align="right">Temperature</TableCell>
+                <TableCell align="right">Weather</TableCell>
+                <TableCell align="right">Icon</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {forecastData.data.list.map((list, index) => (
+                <TableRow
+                  key={index}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
+                  <TableCell component="th" scope="row">
+                    {list.dt_txt}
+                  </TableCell>
+                  <TableCell align="right">{city}</TableCell>
+                  <TableCell align="right">{list.main.temp}</TableCell>
+                  <TableCell align="right">
+                    {list.weather[0].description}
+                  </TableCell>
+                  <TableCell align="right">
+                    <img
+                      src={`https://openweathermap.org/img/wn/${list.weather[0].icon}.png`}
+                      alt="icon"
+                    />
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
+    </>
+  );
+};
